74b936114eb551bce6663a280c9cc6ecd8c21b0f
ruthiguta <cirurue@gmail.com>
2026-03-27 19:17:27 +0300

    update url to solve a cors issue

--- app/__init__.py
+++ app/__init__.py
@@ -7,17 +7,18 @@ def create_app():
     app = Flask(__name__)
 
     # Configure CORS
-    CORS(
-        app,
-        origins=[
-            "http://localhost:5173",
-            "http://localhost:5174",
-            "https://ai-wrapper-frontend-zrf5.onrender.com",
-            "https://ai-wrapper-frontend-sooty.vercel.app/",
-        ],
-        supports_credentials=True,
-        allow_headers=["Content-Type", "Authorization"],
-    )
+    CORS(app, origins="*", supports_credentials=True)
+    # CORS(
+    #     app,
+    #     origins=[
+    #         "http://localhost:5173",
+    #         "http://localhost:5174",
+    #         "https://ai-wrapper-frontend-zrf5.onrender.com",
+    #         "https://ai-wrapper-frontend-sooty.vercel.app/",
+    #     ],
+    #     supports_credentials=True,
+    #     allow_headers=["Content-Type", "Authorization"],
+    # )
 
     # Register blueprints
     from app.api.v1.ai import ai_bp
